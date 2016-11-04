ebb336a59ac719980ab3ca0b3bfa582d1ec215b9
Ritza Kufita Rizqia <ritzakufita@gmail.com>
2016-11-04 10:46:09 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/MainActivity.java
@@ -25,8 +25,10 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
     public static final String HOTEL="hotel";
     public static final int REQUEST_CODE_ADD = 88;
+    public static final int REQUEST_CODE_EDIT = 99;
     ArrayList<Hotel> mList = new ArrayList<>();
     HotelAdapter mAdapter;
+    int itemPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,6 +66,13 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
             mList.add(hotel);
             mAdapter.notifyDataSetChanged();
         }
+        else if (requestCode == REQUEST_CODE_EDIT && resultCode == RESULT_OK)
+        {
+            Hotel hotel = (Hotel) data.getSerializableExtra(HOTEL);
+            mList.remove(itemPos);
+            mList.add(itemPos,hotel);
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
     private void goAdd()
@@ -92,7 +101,7 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
 
         for(int i = 0; i < arJudul.length; i++)
         {
-            mList.add(new Hotel(arJudul[i],arDeskripsi[i],arFoto[i], arDetail[i],arLokasi[i]));
+            mList.add(new Hotel(arJudul[i],arDeskripsi[i], arDetail[i], arLokasi[i], arFoto[i] ));
         }
         mAdapter.notifyDataSetChanged();
     }
@@ -124,5 +133,28 @@ public class MainActivity extends AppCompatActivity implements HotelAdapter.IHot
         Intent intent = new Intent(this, DetailActivity.class);
         intent.putExtra(HOTEL,mList.get(pos));
         startActivity(intent);
+    }
+
+    @Override
+    public void doEdit(int pos) {
+        itemPos = pos;
+        Intent intent = new Intent(this, InputActivity.class);
+        intent.putExtra(HOTEL, mList.get(pos));
+        startActivityForResult(intent, REQUEST_CODE_EDIT);
+    }
+
+    @Override
+    public void doDelete(int pos) {
+
+    }
+
+    @Override
+    public void doFav(int pos) {
+
+    }
+
+    @Override
+    public void doShare(int pos) {
+
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -11,15 +11,15 @@ public class Hotel implements Serializable{
     public String judul;
     public String deskripsi;
     public String detail;
-    public String lokasi;
     public String foto;
+    public String lokasi;
 
-    public Hotel(String judul, String deskripsi, String foto, String detail, String lokasi)
+    public Hotel(String judul, String deskripsi, String detail, String lokasi, String foto)
     {
         this.judul=judul;
         this.deskripsi=deskripsi;
         this.detail= detail;
-        this.lokasi = lokasi;
         this.foto = foto;
+        this.lokasi = lokasi;
     }
 }
