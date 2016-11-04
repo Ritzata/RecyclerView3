41feb299822b199ff53e59ed90cead94e2415f42
Ritza Kufita Rizqia <ritzakufita@gmail.com>
2016-11-04 08:11:28 +0700

    RecyclerView View Detail

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/model/Hotel.java
@@ -2,18 +2,24 @@ package id.sch.smktelkom_mlg.learn.recyclerview3.model;
 
 import android.graphics.drawable.Drawable;
 
+import java.io.Serializable;
+
 /**
  * Created by LENOVO Z40-75 on 02/11/2016.
  */
-public class Hotel {
+public class Hotel implements Serializable{
     public String judul;
     public String deskripsi;
-    public Drawable foto;
+    public String foto;
+    public String detail;
+    public String lokasi;
 
-    public Hotel(String judul, String deskripsi, Drawable foto)
+    public Hotel(String judul, String deskripsi, String foto, String detail, String lokasi)
     {
         this.judul=judul;
         this.deskripsi=deskripsi;
+        this.detail= detail;
+        this.lokasi = lokasi;
         this.foto = foto;
     }
 }
